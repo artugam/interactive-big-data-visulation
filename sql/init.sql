af79736883ad45526b45f55fdec355574d753ad5
Artur Pilch <apilch@g2a.com>
2020-11-12 20:58:36 +0100

    feat: sql

---
+++ sql/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS checkins (
+    space_layer integer,
+    time_layer integer,
+    tile_x bigint,
+    tile_y bigint,
+    "time" integer,
+    cnt bigint,
+    cnt_log double precision
+);
+
+COPY checkins FROM '/var/lib/postgress/checkins-little.csv' WITH DELIMITER E'\t' CSV HEADER;
